012a3ef0d02d0beb9a673c08fba47ddeb337e905
dan <dan@ddiware.com>
2022-12-05 22:27:36 +0000

    added TS modules

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,8 @@ import './App.css';
 import { store, persistor } from './services/redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root') as HTMLElement) ;
+
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
