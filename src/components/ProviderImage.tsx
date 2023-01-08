ca4ac15be31151fc030497c755b503784bacca3c
dan <dan@ddiware.com>
2023-01-08 17:34:56 +0000

    defined services connection modal

--- src/components/ProviderImage.tsx
+++ src/components/ProviderImage.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
 import * as images from '../assets/index';
+import { SemanticFLOATS, SemanticSIZES } from 'semantic-ui-react';
 
-import { IProviderImage } from '../types';
+interface IProps {
+  provider: string;
+  size: SemanticSIZES;
+  floated: SemanticFLOATS;
+}
 
-export const ProviderImage = (props: IProviderImage) => {
-  const Providers = {
+export const ProviderImage = (props: IProps) => {
+  const Providers: Record<string, keyof typeof Image> = {
     AWS: images.AWS96,
     Azure: images.AZURE96,
     Google: images.GOOGLE96,
@@ -13,14 +18,9 @@ export const ProviderImage = (props: IProviderImage) => {
     Microsoft: images.MICROSOFT96,
   };
 
-  const image = Providers[props.provider] ;
+  const image = Providers[props.provider];
 
   return (
-    <Image
-      src={image}
-      size={props.size}
-      alt="ServiceProviderLogo"
-      floated={props.floated}
-    />
+    <Image src={image} size={props.size} alt="ServiceProviderLogo" floated={props.floated} style={{ margin: 0 }} />
   );
 };
--- src/components/connections/AddServiceConnectionAccordion.js
+++ src/components/connections/AddServiceConnectionAccordion.js
@@ -1,14 +1,12 @@
-import React, { useReducer, useState } from 'react';
-import { Card, Button, Divider } from 'semantic-ui-react';
-import { ServiceConnectionPage, ButtonGroup } from '../../styles/StyledServiceConnections';
-import { PageTitle } from '../PageTitle';
-import { ServiceConnections as ServiceConnectionCards } from './ServiceConnections';
+/* Superceeded */
+
+import React, { useState } from 'react';
+import { Card, Button } from 'semantic-ui-react';
+import { ButtonGroup } from '../../styles/StyledServiceConnections';
 import ServiceConnectionTable from './ServiceConnectionTable';
-import { ProviderImage } from '../ProviderImage';
 import AddServiceAzure from './AddServiceAzure';
 import AddServiceAWS from './AddServiceAWS';
 import ValidateServiceButton from '../buttons/ValidateServiceButton';
-import ServiceConnectionModal from './ServiceConnectionModal';
 
 const ExtraContentAccordionClosed = ({ content, onClick }) => {
   return (
@@ -18,7 +16,6 @@ const ExtraContentAccordionClosed = ({ content, onClick }) => {
       </Card.Content>
       <Card.Content extra>
         <Button onClick={onClick}>Accordian</Button>
-        <ServiceConnectionModal />
       </Card.Content>
     </>
   );
@@ -78,36 +75,4 @@ function cardStateReducer(state, { type, payload }) {
   return state;
 }
 
-const ServiceConnection = () => {
-  const [state, dispatch] = useReducer(cardStateReducer, [false]);
-
-  function toggleCard(card) {
-    return function () {
-      dispatch({ type: 'TOGGLE', payload: { card } });
-    };
-  }
-
-  return (
-    <ServiceConnectionPage>
-      <PageTitle title="Service Connections" />
-      <Divider />
-      <Card.Group itemsPerRow={2}>
-        {ServiceConnectionCards.filter((card) => card.active).map((card, index) => {
-          return (
-            <Card key={index} color={card.color} style={{ height: '100%' }}>
-              <Card.Content>
-                <ProviderImage floated="left" provider={card.provider} size="tiny" />
-                <Card.Header>{card.name}</Card.Header>
-                <Card.Meta>{card.description}</Card.Meta>
-                <Card.Meta>{card.details}</Card.Meta>
-              </Card.Content>
-              <ExtraContentAccordion content={card} onToggle={toggleCard(index)} open={state[index]} />
-            </Card>
-          );
-        })}
-      </Card.Group>
-    </ServiceConnectionPage>
-  );
-};
-
-export default ServiceConnection;
+export default ExtraContentAccordion;
