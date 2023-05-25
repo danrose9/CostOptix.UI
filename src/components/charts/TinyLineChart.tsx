85207fbf7296befdc2cab6affa2749013161a655
dan <dan@ddiware.com>
2023-05-25 16:50:10 +0100

    TinyLineChart update

--- src/components/charts/TinyLineChart.tsx
+++ src/components/charts/TinyLineChart.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import { LineChart, Line } from 'recharts';
 
-type ConfigType = {
+export interface ITinyLineChartProps {
   width: number;
   height: number;
-  color?: string;
-};
-
-type PropsType = {
   data: any;
-  config?: ConfigType;
-};
+  dataKey: string;
+}
 
-const TinyLineChart = ({ data, config }: PropsType) => {
+const TinyLineChart = (props: ITinyLineChartProps) => {
+  const { dataKey, width, height, data } = props;
   return (
-    <LineChart
-      width={config?.width}
-      height={config?.height}
-      data={data}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    >
-      <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={3} dot={false} />
+    <LineChart width={width} height={height} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+      <Line type="monotone" dataKey={dataKey} stroke="#8884d8" strokeWidth={3} dot={false} />
     </LineChart>
   );
 };
