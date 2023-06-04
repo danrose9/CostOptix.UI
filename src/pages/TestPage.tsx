736db2dd9860b4684b21dc97dc53c3452c840eef
dan <dan@ddiware.com>
2023-06-04 12:34:43 +0100

    test page update

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -17,38 +17,53 @@ const ComponentContainer = styled.div`
 `;
 
 const Box = styled.div<BoxProps>`
-  width: 30vw;
-  height: 10vh;
   margin: 10px;
-  transition: all 0.3s ease-in-out;
-
-  ${(props) =>
-    props.selected &&
-    css`
-      width: 20vw;
-      height: 70vh;
-    `}
+  transition: all 1s ease-in-out;
+  cursor: pointer;
 `;
 
 const Box1 = styled(Box)`
   border: 1px solid red;
+
+  width: 40vw;
+  height: 10vh;
+  ${(props) =>
+    props.selected &&
+    css`
+      width: 30vw;
+      height: 70vh;
+    `}
 `;
 
 const Box2 = styled(Box)`
   border: 1px solid blue;
+
+  width: 40vw;
+  height: 10vh;
+  ${(props) =>
+    props.selected &&
+    css`
+      width: 30vw;
+      height: 70vh;
+    `}
 `;
 
 interface BoxComponentProps {
   selected: boolean;
   onClick: () => void;
+  children?: React.ReactNode;
 }
 
 const Box1Component: React.FC<BoxComponentProps> = ({ selected, onClick }) => (
-  <Box1 onClick={onClick} selected={selected} />
+  <Box1 onClick={onClick} selected={selected}>
+    <h3>List</h3>
+  </Box1>
 );
 
 const Box2Component: React.FC<BoxComponentProps> = ({ selected, onClick }) => (
-  <Box2 onClick={onClick} selected={selected} />
+  <Box2 onClick={onClick} selected={selected}>
+    <h3>Detail</h3>
+  </Box2>
 );
 
 function TestPage() {
@@ -62,6 +77,7 @@ function TestPage() {
     <PageLayout title="Cost Containers">
       <ComponentContainer>
         <Box1Component onClick={() => handleClick('box1')} selected={selectedBox === 'box1'} />
+
         <Box2Component onClick={() => handleClick('box2')} selected={selectedBox === 'box2'} />
       </ComponentContainer>
     </PageLayout>
