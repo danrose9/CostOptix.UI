167cb5ce9d299c7dd72b85613c95f7881027aaba
dan <dan@ddiware.com>
2023-06-06 17:00:45 +0100

    started on query filter

--- src/pages/components/QueryFilter.tsx
+++ src/pages/components/QueryFilter.tsx
@@ -21,57 +21,103 @@ const stringOperators = [
   { key: 6, text: 'ends with', value: 6 },
 ];
 
-const QueryFilter: React.FC<IQueryFilterProps> = (props) => {
-  const [filterGroup, setFilterGroup] = useState<any>([]);
-
-  const onAddBtnClick = (event: any) => {
-    setFilterGroup(filterGroup.concat(<FilterGroup key={filterGroup.length} />));
-  };
-
-  return (
-    <PageLayout title="Query Filter">
-      <div style={{ height: '20px' }} />
-      <Button onClick={onAddBtnClick}>Add input</Button>
-      <div style={{ height: '20px' }} />
-      {filterGroup}
-    </PageLayout>
-  );
-};
-
+const StyledResult = styled.div`
+  display: block;
+  font-family: monospace;
+  white-space: pre;
+  margin: 1em 0;
+`;
 const StyledInput = styled(Input)`
   border: 1px;
 `;
 
 const StyledFilterGroup = styled.div`
-  display: flex;
+  padding: 1em;
 `;
 
 const StyledActionGroup = styled(Button.Group)`
   padding: 5px;
 `;
 
-const FilterGroup: React.FC<any> = () => {
+const StyledDropdown = styled(Dropdown)`
+  z-index: 1;
+
+  &:after {
+    z-index: 1000;
+  }
+`;
+
+const FilterOutput: React.FC<any> = (props) => {
+  const { value } = props;
+  return (
+    <Grid columns={1}>
+      <Grid.Column>
+        <Segment secondary>
+          <StyledResult>Query: {value}</StyledResult>
+        </Segment>
+      </Grid.Column>
+    </Grid>
+  );
+};
+const FilterGroup: React.FC<any> = ({ index, onRemoveBtnClick, onAddBtnClick }) => {
   const [value, setValue] = React.useState();
 
   const handleChange = (e: any, { value }: any) => setValue(value);
   return (
     <Grid columns={1}>
       <StyledFilterGroup>
-        <Dropdown onChange={handleChange} options={fields} placeholder="Select field" selection value={value} search />
-        <Dropdown options={stringOperators} placeholder="Select operator" selection search />
-        <StyledInput placeholder="Enter filter value" />
+        <StyledDropdown
+          onChange={handleChange}
+          options={fields}
+          placeholder="Select field"
+          selection
+          value={value}
+          search
+          style={{ margin: '0 5px' }}
+        />
+        <StyledDropdown
+          options={stringOperators}
+          placeholder="Select operator"
+          selection
+          search
+          style={{ margin: '0 5px' }}
+        />
+        <StyledInput placeholder="Enter filter value" style={{ margin: '0 5px' }} />
         <StyledActionGroup size="mini">
-          <Button icon="add" />
-          <Button icon="close" />
+          <Button icon="add" onClick={onAddBtnClick} />
+
+          <Button icon="close" onClick={() => onRemoveBtnClick(index)} />
         </StyledActionGroup>
       </StyledFilterGroup>
-      <Grid.Column>
-        <Segment secondary>
-          <pre>Query: {value}</pre>
-        </Segment>
-      </Grid.Column>
     </Grid>
   );
 };
 
+const QueryFilter: React.FC<IQueryFilterProps> = (props) => {
+  const [filterGroup, setFilterGroup] = useState<any>([]);
+  const [filterOutput, setFilterOutput] = useState<any>([]);
+
+  const onAddBtnClick = (event: any) => {
+    const newFilterGroup = <FilterGroup key={filterGroup.length} onRemoveBtnClick={onRemoveBtnClick} />;
+    setFilterGroup([...filterGroup, newFilterGroup]);
+  };
+
+  const onRemoveBtnClick = (index: number) => {
+    const updatedFilterGroup = filterGroup.filter((_: any, i: number) => i !== index);
+    setFilterGroup(updatedFilterGroup);
+  };
+
+  return (
+    <PageLayout title="Query Filter">
+      <div style={{ padding: '20px 0 ' }}>
+        <Button onClick={onAddBtnClick}>Add input</Button>
+      </div>
+      {filterGroup.map((filter: any, index: any) => (
+        <FilterGroup key={index} index={index} onRemoveBtnClick={onRemoveBtnClick} onAddBtnClick={onAddBtnClick} />
+      ))}
+
+      <FilterOutput value={filterOutput} />
+    </PageLayout>
+  );
+};
 export default QueryFilter;
