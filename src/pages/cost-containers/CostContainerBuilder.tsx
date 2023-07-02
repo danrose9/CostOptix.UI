48f63b40bff765f100f4f3bfffcaf2f3dc8fd114
dan <dan@ddiware.com>
2023-07-02 18:33:16 +0100

    Added Query Filter container to page

--- src/pages/cost-containers/CostContainerBuilder.tsx
+++ src/pages/cost-containers/CostContainerBuilder.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Button, Modal, Table, Icon } from 'semantic-ui-react';
+import { Button, Modal, Table, Icon, Container, Segment } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 const AddNewContainerRow = styled(Table.Row)`
@@ -9,39 +9,9 @@ const AddNewContainerRow = styled(Table.Row)`
 
 export interface ICostContainerBuilderProps {}
 
-const AddNewContainer = () => {
-  return (
-    <>
-      <Table.Cell width="16">
-        <Icon name="add" size="large" /> Add Container
-      </Table.Cell>
-    </>
-  );
-};
-
 export function CostContainerBuilder(props: ICostContainerBuilderProps) {
   const [open, setOpen] = React.useState(false);
-  return (
-    <Modal
-      centered={false}
-      open={open}
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-      trigger={
-        <AddNewContainerRow>
-          <AddNewContainer />
-        </AddNewContainerRow>
-      }
-    >
-      <Modal.Header>Container Query Builder</Modal.Header>
-      <Modal.Content>
-        <Modal.Description>Create a new Cost Container</Modal.Description>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button onClick={() => setOpen(false)}>OK</Button>
-      </Modal.Actions>
-    </Modal>
-  );
+  return <Segment>Pellentesque habitant morbi tristique senectus.</Segment>;
 }
 
 export default CostContainerBuilder;
