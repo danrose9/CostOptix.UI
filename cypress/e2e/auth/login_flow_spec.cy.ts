08f7f57b15459691e02dc7e774147d63f639ed03
dan <dan@ddiware.com>
2023-10-04 22:45:44 +0100

    update to e2e-testing.yaml

--- cypress/e2e/auth/login_flow_spec.cy.ts
+++ cypress/e2e/auth/login_flow_spec.cy.ts
@@ -6,27 +6,27 @@ describe('Login flow', () => {
     cy.visit('/');
   });
 
-  test('user is signed up and can login successfully when clicking login button', () => {
+  test('#3. user is signed up and can login successfully when clicking login button', () => {
     cy.get('[data-testid="login-ext-button"]').click();
     cy.url().should('eq', `${Cypress.config().baseUrl}${appRoutes.LOGIN}`);
     cy.contains('Continue with Azure').should('be.visible').click();
-    cy.loginToAAD(Cypress.env('aad_username'), Cypress.env('aad_password'));
+    cy.loginToAAD(Cypress.env('aad_reddogdev_username_1'), Cypress.env('aad_reddogdev_password_1'));
     cy.url().should('eq', `${Cypress.config().baseUrl}${appRoutes.COST_DASHBOARD}`);
 
     cy.logout();
   });
 
-  test('user is signed up and can login successfully when clicking signup button', () => {
+  test('#4. user is signed up and can login successfully when clicking signup button', () => {
     cy.get('[data-testid="get-started-button"]').click();
     cy.url().should('eq', `${Cypress.config().baseUrl}${appRoutes.SIGNUP}`);
 
-    cy.fillSignUpForm(Cypress.env('aad_username'));
+    cy.fillSignUpForm(Cypress.env('aad_reddogdev_username_1'));
     cy.get('[data-testid="next-button"]').click();
     cy.url().should('eq', `${Cypress.config().baseUrl}${appRoutes.LOGIN}`);
     cy.contains('Continue with Azure').should('be.visible').click();
-    cy.loginToAAD(Cypress.env('aad_username'), Cypress.env('aad_password'));
+    cy.loginToAAD(Cypress.env('aad_reddogdev_username_1'), Cypress.env('aad_reddogdev_password_1'));
     cy.url().should('eq', `${Cypress.config().baseUrl}${appRoutes.COST_DASHBOARD}`);
-    cy.contains(Cypress.env('aad_userfullname')).should('be.visible');
+    cy.contains(Cypress.env('aad_reddogdev_fullname_1')).should('be.visible');
     cy.logout();
   });
 });
