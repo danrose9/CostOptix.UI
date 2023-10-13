bb103ce173570488a278df193d8db03c0b5b6d30
dan <dan@ddiware.com>
2023-10-13 11:40:49 +0100

    0.4.82

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,6 @@ import { Provider } from 'react-redux';
 import './App.css';
 import { store, persistor } from './services/redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import './web.config';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
