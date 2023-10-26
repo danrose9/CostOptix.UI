64083273a0e24b4b4a425785ce378a1d848d4c04
dan <dan@ddiware.com>
2023-10-26 16:10:31 +0100

    fixed tests

--- src/components/pages/__tests__/Page404.test.tsx
+++ src/components/pages/__tests__/Page404.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { PageNotFound404 } from '../pages/index';
+import { PageNotFound404 } from '../index';
 
 import renderer from 'react-test-renderer';
 
