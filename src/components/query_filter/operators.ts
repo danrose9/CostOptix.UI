3df5686a977662440c1891a583b13a0b20f42dd7
ianByrne <ian.byrne@burnsie.com.au>
2023-11-05 16:37:12 +0000

    Add additional operators

--- src/components/query_filter/operators.ts
+++ src/components/query_filter/operators.ts
@@ -44,10 +44,10 @@ export const operators = [
   { key: 4, text: 'greater than or equal', value: 'gte' },
   { key: 5, text: 'less than', value: 'lt' },
   { key: 6, text: 'less than or equal', value: 'lte' },
-  // { key: 7, text: 'contains', value: 'contains' },
-  // { key: 8, text: 'does not contain', value: 'ncontains' },
-  // { key: 9, text: 'begins with', value: 'beginWith' },
-  // { key: 10, text: 'ends with', value: 'endWith' },
+  { key: 7, text: 'contains', value: 'contains' },
+  { key: 8, text: 'does not contain', value: 'ncontains' },
+  { key: 9, text: 'begins with', value: 'beginWith' },
+  { key: 10, text: 'ends with', value: 'endWith' },
 ];
 
 export const conditionalOperators = [
