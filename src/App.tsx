e63251714e988d32f0bd364bafc665dac2d1c5f2
dan <dan@ddiware.com>
2024-01-15 13:23:13 +0000

    feat: google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ApplicationContextProvider } from './app/ApplicationContext';
-
 import AppContent from './app/AppContent';
-import ReactGA from 'react-ga';
-
-const TRACKING_ID = 'G-6KPES2WMQB';
-ReactGA.initialize(TRACKING_ID);
 
 export const App = () => {
   sessionStorage.setItem('application version', process.env.REACT_APP_VERSION as string);
