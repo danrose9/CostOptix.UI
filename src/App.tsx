3dc599a2dee651678adb9c7e942364581b07e475
dan <dan@ddiware.com>
2024-01-15 13:02:07 +0000

    feat: added google analytics to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ApplicationContextProvider } from './app/ApplicationContext';
 
 import AppContent from './app/AppContent';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = 'G-6KPES2WMQB';
+ReactGA.initialize(TRACKING_ID);
 
 export const App = () => {
   sessionStorage.setItem('application version', process.env.REACT_APP_VERSION as string);
