c04aff74feb94ee36899d93b43cc907ee57280d5
dan <dan@ddiware.com>
2024-03-10 16:07:45 +0000

    updated azure steps

--- src/components/forms/AzureCustomerAgreement.tsx
+++ src/components/forms/AzureCustomerAgreement.tsx
@@ -5,37 +5,29 @@ export const AzureCustomerAgreement = () => {
   return (
     <>
       <Form.Field>
-        16. Navigate to <b>Subscriptions</b>
-      </Form.Field>
-      <Form.Field>17. For each subscription that you would like to report on complete steps 18-21</Form.Field>
-      <Form.Field>
-        <Form.Field>18. Select a subscription</Form.Field>
-        19. Select <i>Access control IAM</i> followed by <i>Role Assignments</i>
-      </Form.Field>
-      <Form.Field>
-        20. Click <i>Add</i> followed by <i>Add role assignment</i>
-      </Form.Field>
-      <Form.Field>
-        21. Choose <i>Reader</i> and add the app you created earlier as a <i>member</i>
+        Some customer may have a Microsoft Customer Agreement tenant type, if you do, then we'll just need to complete a
+        few more steps. <br /> <br />
+        If you are unsure you can check by navigating to
+        <a href="https://portal.azure.com/#view/Microsoft_Azure_GTM/ModernBillingMenuBlade/~/AllBillingScopes">
+          {' '}
+          Cost Management.
+        </a>{' '}
+        <br /> <br />
+        Each of your billing scopes will show a <i>Billing account type</i>. If the billing account you would like to
+        report on is a <i>Microsoft Customer Agreement</i> type then continue with the rest of these steps.
       </Form.Field>
       <Form.Field>
-        22. Once completed click <i>Review + Assign</i>
-      </Form.Field>
-      <Form.Field>
-        23. Navigate to <b>Cost Management + Billing</b>.
-      </Form.Field>
-      <Form.Field>
-        24. Select <i>Billing scopes</i> from the left hand Nav, select the <i>billing account</i> you would like to
+        19. Select <i>Billing scopes</i> from the left hand Nav, select the <i>billing account</i> you would like to
         report on.
       </Form.Field>
       <Form.Field>
-        25. Select <i>Access control (IAM)</i> from the left hand Nav
+        20. Select <i>Access control (IAM)</i> from the left hand Nav
       </Form.Field>
       <Form.Field>
-        26. Select <i>Add</i>
+        21. Select <i>Add</i>
       </Form.Field>
       <Form.Field>
-        27. Select <i>Billing account reader</i>. Select the App you created earlier and click <b>Add</b>
+        22. Select <i>Billing account reader</i>. Select the App you created earlier and click <b>Add</b>
       </Form.Field>
     </>
   );
