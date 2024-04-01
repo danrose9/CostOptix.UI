cf9f8d7bf6d6887ea243b565def33b54d7e0e9e6
dan <dan@ddiware.com>
2024-04-01 16:42:50 +0100

    added Feedback.test

---
+++ src/components/__tests__/Feedback.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Feedback, FEEDBACK_PLACEHOLDER, FEEDBACK_LABEL } from '../Feedback';
+import * as api from 'src/services/api/submitFeedback';
+
+// Mock the submitFeedback API call
+jest.mock('src/services/api/submitFeedback', () => ({
+  submitFeedback: jest.fn(),
+}));
+
+describe('Feedback Component', () => {
+  beforeEach(() => {
+    // Clear all instances and calls to constructor and all methods:
+    (api.submitFeedback as jest.Mock).mockClear();
+  });
+
+  test('initial render and open feedback form', () => {
+    const { getByText, queryByText } = render(<Feedback />);
+    expect(queryByText('Submit')).not.toBeInTheDocument();
+
+    fireEvent.click(getByText(FEEDBACK_LABEL));
+    expect(getByText('Submit')).toBeInTheDocument();
+  });
+
+  test('submitting feedback', async () => {
+    const { getByText, getByPlaceholderText, container, getByTestId, queryByText } = render(<Feedback />);
+
+    fireEvent.click(getByText(FEEDBACK_LABEL));
+
+    const ratingSelector = getByTestId('rating-selector');
+    const fifthStar = ratingSelector.querySelector('i[aria-posinset="5"]');
+    if (fifthStar) {
+      fireEvent.click(fifthStar);
+    }
+
+    // Simulate user input
+    fireEvent.change(getByPlaceholderText(FEEDBACK_PLACEHOLDER), {
+      target: { value: 'Great experience!' },
+    });
+    fireEvent.click(getByText('Submit'));
+
+    // Mock successful feedback submission
+    (api.submitFeedback as jest.Mock).mockResolvedValue({ success: true });
+
+    expect(api.submitFeedback).toHaveBeenCalledWith({
+      rating: 5,
+      feedbackText: 'Great experience!',
+    });
+
+    expect(api.submitFeedback).toHaveBeenCalledTimes(1);
+  });
+});
