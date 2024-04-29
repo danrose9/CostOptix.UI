7fc2bc7ef33ccebe781deda0c8e7398e1c99d738
dan <dan@ddiware.com>
2024-04-29 21:12:25 +0100

    scaffold new home page

---
+++ src/components/ContactUs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageWrapper from './pages/PageWrapper';
+import { PageSection } from './pages/DefaultPageStyles';
+
+export const ContactUs = () => {
+  return (
+    <PageWrapper>
+      <PageSection>Contact Page</PageSection>
+    </PageWrapper>
+  );
+};
+
+export default ContactUs;
